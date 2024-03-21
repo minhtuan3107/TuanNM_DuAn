55f9e451a286b64d000cd8c65d20ade88eb102cd
Nguyễn Minh Tuấn <tn989993@gmail.com>
2024-03-21 08:34:58 +0700

    TuanNM demo code

---
+++ BE/src/main/java/org/example/be/model/Account.java
@@ -0,0 +1,40 @@
+package org.example.be.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String accountName;
+    private String fullName;
+    private String password;
+    private String facebookId;
+
+    private String googleId;
+    @Column(columnDefinition = "LONGTEXT")
+    private String profilePicture;
+
+    private LocalDate birthday;
+
+    private String email;
+    private String address;
+    private String phone;
+    @Column(columnDefinition = "boolean default false")
+    private Boolean isDeleted = false;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id", referencedColumnName = "id")
+    private Role role;
+}
---
+++ BE/src/main/java/org/example/be/service/IAccountService.java
@@ -0,0 +1,5 @@
+package org.example.be.service;
+
+public interface IAccountService {
+
+}
