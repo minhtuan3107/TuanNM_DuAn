778f282d19957af1587c9a129c41c10f9599ad1c
Nguyễn Minh Tuấn <tn989993@gmail.com>
2024-04-11 15:34:54 +0700

    TUANNM up code

--- BE/src/main/java/com/example/backendglasses/config/Main.java
+++ BE/src/main/java/com/example/backendglasses/config/Main.java
@@ -1,15 +1,10 @@
 package com.example.backendglasses.config;
 
-import java.util.Stack;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {4, 5, 6, 2, 8, 9};
-        int key = 2;
-        for (int i = 0; i < arr.length; i++) {
-            if (key == arr[i]) ;
-            System.out.println(key + " o vi tri: " + i);
-            break;
-        }
+      List<String> a = new ArrayList<>();
+
     }
 }
